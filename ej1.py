b98c51bf2a25b028eb9ac45e49d9e013eefa43d8
FranGiayetto <frangiayetto@gmail.com>
2022-04-22 12:04:08 -0300

    ejercicio1 de la guia2

---
+++ ej1.py
@@ -0,0 +1,21 @@
+class persona():
+    def inicializar(self, nombre):
+        self.nombre=nombre
+    def imprimir(self):
+        try:
+            print("Nombre: ", self.nombre)
+        except AttributeError:
+            print("No iniciaste la persona")
+
+
+persona1=persona()
+persona1.inicializar("Juan")
+persona1.imprimir()
+
+persona2=persona()
+persona2.inicializar("Marcos")
+persona2.imprimir()
+
+persona3=persona()
+persona3.imprimir()
+
