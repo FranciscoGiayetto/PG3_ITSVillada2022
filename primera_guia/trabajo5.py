93bfc0bb46c3413b5d180e9f18a1acac961a8f9d
FranGiayetto <frangiayetto@gmail.com>
2022-04-29 09:44:04 -0300

    Primer Guia

---
+++ primera_guia/trabajo5.py
@@ -0,0 +1,6 @@
+palabra= input("ingrese la plabra: ")
+vuelta= "".join(reversed(palabra))
+if(palabra ==  vuelta):
+    print("su palabra es capicua ")
+else:
+    print("su palabra no es capicua ")
