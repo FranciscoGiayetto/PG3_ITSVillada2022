d806543b21f53494d6687406ed82fc7cb4be9ca1
FranGiayetto <frangiayetto@gmail.com>
2022-09-16 11:59:10 -0300

    ✨✨

--- Django/proyecto1/app1/admin.py
+++ Django/proyecto1/app1/admin.py
@@ -3,15 +3,42 @@ from django.contrib import admin
 from app1.models import Categoria, Producto, Venta,Direccion_cliente,Direccion_proveedor,Cliente,Proveedor,Telefonos_clientes,Producto_venta
 
 # Register your models here.
-search_fields=[Categoria.nombre]
+admin.site.site_header='SUPERMERCADO'
+admin.site.site_title='SUPERMERCADO'
 
-admin.site.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display =['nombre','descripcion'] 
+    search_fields=['nombre']
+admin.site.register(Categoria, CategoriaAdmin)
+
+
 admin.site.register(Producto_venta)
-admin.site.register(Producto)
-admin.site.register(Proveedor)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display =['nombre','precio','stock'] 
+    list_filter=('nombre','precio','stock')
+admin.site.register(Producto,ProductoAdmin)
+
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display =['rut','nombre','direccion','web','telefono'] 
+admin.site.register(Proveedor,ProveedorAdmin)
+
+
 admin.site.register(Venta)
-admin.site.register(Direccion_cliente)
-admin.site.register(Direccion_proveedor)
-admin.site.register(Cliente)
-admin.site.register(Telefonos_clientes)
+
+class Direccion_clienteAdmin(admin.ModelAdmin):
+    list_display =['calle','numero','comuna','ciudad'] 
+admin.site.register(Direccion_cliente, Direccion_clienteAdmin)
+
+class Direccion_proveedorAdmin(admin.ModelAdmin):
+    list_display =['calle','numero','comuna','ciudad'] 
+admin.site.register(Direccion_proveedor, Direccion_proveedorAdmin)
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display =['rut','nombre','direccion','telefono'] 
+admin.site.register(Cliente, ClienteAdmin)
+
+class Telefonos_clienteAdmin(admin.ModelAdmin):
+    list_display =['telefono'] 
+admin.site.register(Telefonos_clientes,Telefonos_clienteAdmin)
 
