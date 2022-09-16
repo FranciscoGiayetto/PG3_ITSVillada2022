4b03f3f6681f00f73685de60164179f632def078
FranGiayetto <frangiayetto@gmail.com>
2022-09-16 10:13:30 -0300

    ✨✨

--- Django/proyecto1/app1/admin.py
+++ Django/proyecto1/app1/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from app1.models import Categoria, Producto, Venta,Direccion_cliente,Direccion_proveedor,Cliente,Proveedor,Telefonos_clientes,Producto_venta
+
 # Register your models here.
+search_fields=[Categoria.nombre]
+
+admin.site.register(Categoria)
+admin.site.register(Producto_venta)
+admin.site.register(Producto)
+admin.site.register(Proveedor)
+admin.site.register(Venta)
+admin.site.register(Direccion_cliente)
+admin.site.register(Direccion_proveedor)
+admin.site.register(Cliente)
+admin.site.register(Telefonos_clientes)
+
--- Django/proyecto1/app1/models.py
+++ Django/proyecto1/app1/models.py
@@ -1,26 +1,34 @@
-from pyexpat import model
 from django.db import models
+import datetime
 
 # Create your models here.
 
 class Categoria (models.Model):
 	nombre = models.CharField(max_length= 30)
 	descripcion = models.CharField(max_length= 100)
+	def __str__(self):
+		return self.nombre
 
 class Producto (models.Model):
 	nombre= models.CharField(max_length=30)
 	precio= models.FloatField()
 	stock= models.IntegerField()
+	def __str__(self):
+		return self.nombre
 
 class Venta (models.Model):
 	descuento=models.FloatField()
 	fecha=models.DateField()
+	def __str__(self):
+		return str(self.id)
 
 class Direccion_proveedor(models.Model):
 	calle=models.CharField(max_length=30)
 	numero=models.CharField(max_length=5)
 	comuna=models.CharField(max_length=100)
 	ciudad=models.CharField(max_length=50)
+	def __str__(self):
+		return str(f"{self.calle} {self.numero}")
 
 class Proveedor (models.Model):
 	rut = models.CharField(max_length=100, primary_key=True)
@@ -28,21 +36,33 @@ class Proveedor (models.Model):
 	web=models.URLField()
 	direccion = models.ForeignKey(Direccion_proveedor,on_delete=models.CASCADE)
 	telefono=models.IntegerField(max_length=10)
+	def __str__(self):
+		return self.nombre
 
 class Telefonos_clientes (models.Model):
 	telefono=models.CharField(max_length=10)
+	def __str__(self):
+		return self.telefono
 
 class Direccion_cliente(models.Model):
 	calle=models.CharField(max_length=30)
 	numero=models.CharField(max_length=5)
 	comuna=models.CharField(max_length=100)
 	ciudad=models.CharField(max_length=50)
+	def __str__(self):
+		return str(f"{self.calle} {self.numero}")
 
 class Producto_venta(models.Model):
 	cantidad=models.IntegerField()
+	Producto = models.ForeignKey(Producto,on_delete=models.CASCADE)
+	Venta = models.ForeignKey(Venta,on_delete=models.CASCADE)
+	def __str__(self):
+		return str(self.id)
 
 class Cliente(models.Model):
 	rut = models.CharField(max_length=100, primary_key=True)
 	nombre=models.CharField(max_length=30)
 	direccion = models.ForeignKey(Direccion_cliente,on_delete=models.CASCADE)
 	telefono= models.ForeignKey(Telefonos_clientes,on_delete=models.CASCADE)
+	def __str__(self):
+		return self.nombre
